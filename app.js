06849425b217a0f9887bc87e57f86d1453955900
vrkotesh77 <koteswararao.vema-e@kore.com>
2024-08-08 12:12:31 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var Application = require("./lib/app");
+var Server      = require("./lib/server");
+var sdk         = require("./lib/sdk");
+var config      = require("./config");
+
+var app    = new Application(null, config);
+var server = new Server(config, app);
+
+server.start();
+
+sdk.registerBot(require('./FindAFlight.js'));
+sdk.registerBot(require('./SimpleConversationalBot.js'));
+sdk.registerBot(require('./SimpleConversationalBotWithMultipleBotId.js'));
+sdk.registerBot(require('./GuessTheNumber.js'));
+sdk.registerBot(require('./BookACab.js'));
+sdk.registerBot(require('./OrderAPizza.js'));
+sdk.registerBot(require('./BotVariables.js'));
+sdk.registerBot(require('./LiveChat.js'));
